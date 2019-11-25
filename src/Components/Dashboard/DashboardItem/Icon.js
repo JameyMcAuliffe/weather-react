922483e18d1edc31f2c7d4a2a6cf1a61ee8cb3ee
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2019-11-25 17:57:42 -0500

    moved background images to new file, fixed icon bug

--- src/Components/Dashboard/DashboardItem/Icon.js
+++ src/Components/Dashboard/DashboardItem/Icon.js
@@ -18,37 +18,38 @@ import StormNight from '../../../images/icons/storm_night.png'
 import StormDay from '../../../images/icons/storm_day.png'
 
 const Icon = ({id, day}) => {
+
 	const iconsArray = [
-		{id: '01', img: ClearDayIcon, day: true},
-		{id: '01', img: ClearNightIcon, day: false},
-		{id: '02', img: PartlyCloudyDay, day: true},
-		{id: '02', img: PartlyCloudyNight, day: false},
-		{id: '03', img: CloudsDay, day: true},
-		{id: '03', img: CloudsNight, day: false},
-		{id: '04', img: CloudsHeavyNight, day: false},
-		{id: '04', img: CloudsHeavyDay, day: true},
-		{id: '09', img: RainHeavyDay, day: true},
-		{id: '09', img: RainHeavyNight, day: false},
-		{id: '10', img: RainNight, day: false},
-		{id: '10', img: RainDay, day: true},
-		{id: '11', img: StormDay, day: true},
-		{id: '11', img: StormNight, day: false},
-		{id: '13', img: SnowNight, day: false},
-		{id: '13', img: SnowDay, day: true},
-		{id: '50', img: CloudsNight, day: false},
-		{id: '50', img: CloudsDay, day: true}
+		{id: '01d', img: ClearDayIcon},
+		{id: '01n', img: ClearNightIcon},
+		{id: '02d', img: PartlyCloudyDay},
+		{id: '02n', img: PartlyCloudyNight},
+		{id: '03d', img: CloudsDay},
+		{id: '03n', img: CloudsNight},
+		{id: '04n', img: CloudsHeavyNight},
+		{id: '04d', img: CloudsHeavyDay},
+		{id: '09d', img: RainHeavyDay},
+		{id: '09n', img: RainHeavyNight},
+		{id: '10n', img: RainNight},
+		{id: '10d', img: RainDay},
+		{id: '11d', img: StormDay},
+		{id: '11n', img: StormNight},
+		{id: '13n', img: SnowNight},
+		{id: '13d', img: SnowDay},
+		{id: '50n', img: CloudsNight},
+		{id: '50d', img: CloudsDay}
 	]
 
 	const [icon, setIcon] = useState();
 
 	useEffect(() => {
 		let selectedIcon = iconsArray.filter(icon => {
-			return icon.id === id.slice(0, -1) && icon.day === day;
+			return icon.id === id;
 		});
 
 		setIcon(selectedIcon[0].img);
-		// eslint-disable-next-line
-	}, []);
+		
+	}, [iconsArray, id]);
 
 	return (
 		<img className="mt-1 mb-3" src={icon} alt=""/>
---
+++ src/images/backgroundImages.js
@@ -0,0 +1,12 @@
+import ClearNight from './clear_night_2.jpg';
+import ClearDay from './clear_2.jpg';
+import CloudyDay from './cloudy.jpeg';
+import CloudyNight from './cloudy_night.jpg';
+import Storm from './storm.jpeg';
+import RainyDay from './rainy_day.jpg';
+import RainyNight from './rainy_night.jpg';
+import Snow from './snow.jpg';
+import MistyNight from './misty_night.jpg';
+import MistyDay from './misty_day.jpg';
+
+export const BackgroundImages = {ClearNight, ClearDay, CloudyDay, CloudyNight, Storm, RainyDay, RainyNight, Snow, MistyNight, MistyDay};
